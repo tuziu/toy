5ba48419711b93b66cf2e0db690f5eb8e599b033
tuz <piotr.tuz@gmail.com>
2012-08-27 00:02:29 +0200

    some refactoring now app crahes

--- uklad.h
+++ uklad.h
@@ -108,24 +108,6 @@ public:
 	}
 };
 
-class Box
-{
-private:
-    size_t m_num_of_it;
-    UkladEwol m_u;
-    std::vector<Result> m_r;
 
-public:
-    Box(size_2t u_size,size_t nu_i, double p=0.5);
-
-private:
-    Box();
-    Box(const Box&);
-    Box& operator = (const Box& );
-
-public:
-    std::vector<Result> grind();
-
-};
 #endif
 
